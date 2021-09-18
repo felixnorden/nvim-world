bb6d701394bd26651fc7acfb8838ab6082a8b61f
Felix Nordén <felixnorden@gmail.com>
2021-09-18 15:54:46 +0200

    feat: add lua lsp install script

---
+++ language-servers/luainstall.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+brew install ninja
+
+# Fetch server
+git clone https://github.com/sumneko/lua-language-server
+cd lua-language-server
+git submodule --init --recursive
+
+# Build
+cd 3rd/luamake
+compile/install.sh
+cd ../..
+./3rd/luamake/luamake rebuild
+
+echo "Successfully installed Sumneko Lua LSP"
