7bff9c66fffc3c47157e5bee8801ce70446a8097
Felix Nordén <felixnorden@gmail.com>
2023-10-03 22:57:51 +0100

    fix: forgefmt on no errors

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -8,6 +8,11 @@ return {
           args = { "fmt", "--raw", "-" },
           cwd = require("conform.util").root_file({ "foundry.toml" }),
           exit_codes = { 0, 1 },
+          -- Only format when no errors are found
+          condition = function(ctx)
+            local diagnostics = vim.diagnostic.get(ctx.bufnr, { severity = vim.diagnostic.severity.ERROR })
+            return #diagnostics == 0
+          end,
         },
       },
       formatters_by_ft = {
